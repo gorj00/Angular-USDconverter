3904261c0cca0a315a24d738b59c37e7f090a199
gorj00 <os.jangorol@gmail.com>
2019-09-05 00:06:10 +0200

    i18n URL changed in module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -70,5 +70,5 @@ export class AppModule { }
 
 // required for AOT compilation
 export function HttpLoaderFactory(http: HttpClient) {
-  return new TranslateHttpLoader(http);
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
 }
